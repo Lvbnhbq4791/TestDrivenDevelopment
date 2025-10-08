a8fd0cdd16e2dafe562e8ac448797c8c9bee6b13
Dmitriy4791 <diminskiy_4791@mail.ru>
2025-10-08 19:20:35 +0300

    add implemented

--- src/main/java/ru/netology/phonebook/PhoneBook.java
+++ src/main/java/ru/netology/phonebook/PhoneBook.java
@@ -1,7 +1,18 @@
 package ru.netology.phonebook;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
-    public int add(String name, String phoneNumber) {
-        return 0;
+    protected Map<String, String> phoneBookMap;
+
+    public PhoneBook() {
+        this.phoneBookMap = new TreeMap<>();
     }
+
+    public int add(String name, String phoneNumber) {
+        if (!phoneBookMap.containsKey(name)) phoneBookMap.put(name, phoneNumber);
+        return phoneBookMap.size();
+    }
+
 }
